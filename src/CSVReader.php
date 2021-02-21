18969891c97c0e496bd10244f0cc4cae2a7cbcfc
vijaykumar <vijaykumar@drishti-soft.com>
2021-02-21 23:49:21 +0530

    changes

---
+++ src/CSVReader.php
@@ -0,0 +1,62 @@
+<?php
+namespace vizzy\php\util;
+
+use RuntimeException;
+
+class CSVReader
+{
+
+    var $isvalid = false;
+
+    var $actualPath = false;
+
+    var $counter = 0;
+
+    function setPath($path)
+    {
+        if (isset($_FILES[$path])) {
+
+            if ($_FILES[$path]['error'] == '0') {
+                $this->isvalid = true;
+                $this->actualPath = $_FILES[$path]['tmp_name'];
+            }
+        } else {
+            if (file_exists($path)) {
+                $this->isvalid = true;
+                $this->actualPath = $path;
+            } else {
+                throw new RuntimeException("file not found");
+            }
+        }
+    }
+
+    function __construct($path)
+    {
+        $this->setPath($path);
+    }
+
+    function setCounter($counter)
+    {
+        $this->counter = $counter;
+    }
+
+    function read($callbackOb, $callbackMethod)
+    {
+        if ($this->isvalid) {
+            $file = fopen($this->actualPath, "r");
+            $counter = $this->counter;
+            while (! feof($file)) {
+                $data = fgetcsv($file);
+                if ($data) {
+
+                    $counter ++;
+
+                    $callbackOb->{$callbackMethod}($counter, $data);
+                }
+            }
+            fclose($file);
+        }
+
+        return $this->isvalid;
+    }
+}
